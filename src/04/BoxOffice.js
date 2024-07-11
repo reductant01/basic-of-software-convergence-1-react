c7213e2520b0169d90029fe5fef8d6224de5dbef
reductant01 <reductant01@pusan.ac.kr>
2024-07-11 11:34:36 +0900

    라우팅

--- src/04/BoxOffice.js
+++ src/04/BoxOffice.js
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function BoxOffice() {
-  //json data 저장변수
+  // json data 저장변수
   const [tdata, setTdata] = useState([]); // 배열이라서 []사용
   const [tags, setTags] = useState([]);
+  const [selMv, setSelMv] = useState("");
+  const inRef = useRef();
 
-  // 컴포넌트 생성시
-  useEffect(() => {
+  // 데이터 가져오기
+  const getData = (() => {
+    let tmDt = inRef.current.value.replaceAll("-", "")
     let url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?';
     url = url + `key=${process.env.REACT_APP_MV}`;
-    url = url + `&targetDt=20240708`;
+    url = url + `&targetDt=${tmDt}`;
 
     console.log(url);
 
@@ -18,6 +21,31 @@ export default function BoxOffice() {
       .then(resp => resp.json())
       .then(data => setTdata(data.boxOfficeResult.dailyBoxOfficeList))
       ;
+  })
+
+  // 영화가 선택되었을 떄
+  const handleSelMv = ((mv) => {
+    // console.log(mv)
+    let tm = <>
+      <span className="mr-2">{mv.movieNm}</span>
+      <span className="mr-2 text-white">개봉일 : {mv.openDt}</span>
+      <span className="mr-2 text-white">
+        누적관객수 : {parseInt(mv.audiAcc).toLocaleString()}
+      </span>
+    </>
+    setSelMv(tm)
+  })
+
+  // 날짜가 선택되었을 때
+  const handleSelDt = ((e) => {
+    e.preventDefault();
+    console.log(inRef.current.value)
+    getData();
+  })
+
+  // 컴포넌트 생성시
+  useEffect(() => {
+
   }, []);
 
   // tdata가 변경될때 실행
@@ -25,33 +53,34 @@ export default function BoxOffice() {
     if (tdata.length == 0) return;
 
     console.log(tdata)
-    let tm = tdata.map(item => 
-    <tr className="bg-white border-b hover:bg-gray-50 font-bold"
-        key={item.movieCd}> 
+    let tm = tdata.map(item =>
+      <tr className="bg-white border-b hover:bg-gray-50 font-bold hover:cursor-pointer"
+        onClick={() => { handleSelMv(item) }}
+        key={item.movieCd}>
         {/* 왜 key를 입력하지 않으면 오류발생?? */}
-      <th scope="row" className="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        {item.rank}
-      </th>
-      <td className="px-6 py-2">
-        {item.movieNm}
-      </td>
-      <td className="px-6 py-2 text-right">
-        {parseInt(item.salesAmt).toLocaleString()}
-      </td>
-      <td className="px-6 py-2  text-right">
-        {parseInt(item.audiCnt).toLocaleString()}
-      </td>
-      <td className="px-6 py-2">
-        {parseInt(item.rankInten) > 0 ? <span className="text-red-600">▲</span>
-                            : parseInt(item.rankInten) < 0 ? <span className="text-blue-600">▼</span> 
-                            : '-'}
-        {/* if 대신 사용*/}
-        {parseInt(item.rankInten) !=0 && Math.abs(item.rankInten)} 
-        {/* Math.abs = 절댓값 */}
-      </td>
-    </tr>);
+        <th scope="row" className="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          {item.rank}
+        </th>
+        <td className="px-6 py-2">
+          {item.movieNm}
+        </td>
+        <td className="px-6 py-2 text-right">
+          {parseInt(item.salesAmt).toLocaleString()}
+        </td>
+        <td className="px-6 py-2  text-right">
+          {parseInt(item.audiCnt).toLocaleString()}
+        </td>
+        <td className="px-6 py-2">
+          {parseInt(item.rankInten) > 0 ? <span className="text-red-600">▲</span>
+            : parseInt(item.rankInten) < 0 ? <span className="text-blue-600">▼</span>
+              : '-'}
+          {/* if 대신 사용*/}
+          {parseInt(item.rankInten) != 0 && Math.abs(item.rankInten)}
+          {/* Math.abs = 절댓값 */}
+        </td>
+      </tr>);
 
-    setTags(tm) ;  
+    setTags(tm);
 
   }, [tdata]);
 
@@ -59,6 +88,17 @@ export default function BoxOffice() {
     <div className="text-black w-10/12
                     relative overflow-x-auto shadow-md sm:rounded-lg
                     ">
+      <form className="flex justify-end items-center
+                       mb-2
+                       text-lg">
+        <label htmlFor="dt" className="text-sm mr-5 font-bold">
+          날짜선택
+        </label>
+        <input type="date" id="dt"
+          ref={inRef} // input에 ref 속성주기
+          onChange={handleSelDt}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5 " />
+      </form>
       <table className="w-full text-sm text-left rtl:text-right text-gray-500">
         <thead className="text-lg text-gray-50 uppercase bg-black">
           <tr>
@@ -81,10 +121,14 @@ export default function BoxOffice() {
         </thead>
         <tbody>
           {tags}
-          {/* ?? */}
+          {/* tags는 tr을 가지는 코드이다. 바로 실행해도 출력가능 */}
         </tbody>
       </table>
-
+      <div className="flex justify-center items-center
+                      px-6 py-2 font-bold
+                      text-lg bg-black text-yellow-300">
+        {selMv == "" ? "영화정보" : selMv}
+      </div>
 
     </div>
   )
--- src/App.js
+++ src/App.js
@@ -2,41 +2,73 @@
 import './App.css';
 import { FaHome } from "react-icons/fa"; //home 아이콘
 
-// import Hello from './01/Hello' ;
-// import MyDiv1 from './02/MyDiv1';
-// import CardMain from './03/CardMain';
+// 라우팅
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Hello from './01/Hello';
+import MyDiv1 from './02/MyDiv1';
+import CardMain from './03/CardMain';
 import BoxOffice from './04/BoxOffice';
 
 function App() {
   return (
-    <div className='App-header'>
-      <main className='flex flex-col justify-center items-center
+    <BrowserRouter>
+      <div className='App-header'>
+        <main className='flex flex-col justify-center items-center
                      w-full md:w-4/5
                      h-full'>
-        <header className='w-full h-16 bg-slate-200
+          <header className='w-full h-16 bg-slate-200
                            flex justify-between
                            p-5 text-slate-800'>
-          <h1 className='text-xl font-bold'>리액트실습</h1>
-          <div><FaHome /></div>
-        </header>
+            <h1 className='text-xl font-bold'>리액트실습</h1>
+            <ul className='text-lg font-bold
+                         flex justify-center items-center'>
+              <li className='px-5 rounded-sm
+                           hover:text-blue-700 hover:cursor-pointer
+                           '>
+                <Link to="/">clock</Link>
+              </li>
+              <li className='px-5 rounded-sm
+                           hover:text-blue-700 hover:cursor-pointer
+                           '>
+                <Link to='/probs'>probs</Link>
+              </li>
+              <li className='px-5 rounded-sm
+                           hover:text-blue-700 hover:cursor-pointer
+                           '>
+                <Link to='/card'>card</Link>
+              </li>
+              <li className='px-5 rounded-sm
+                           hover:text-blue-700 hover:cursor-pointer
+                           '>
+                <Link to='/box'>BoxOffice</Link>
+              </li>
+            </ul>
+            <div>
+              <Link to='/'><FaHome /></Link>
+            </div>
+          </header>
 
-        <div className="w-full grow
+          <div className="w-full grow
                         flex flex-col justify-center items-center">
-          {/* <Hello />  */}
-          {/* <MyDiv1 /> */}
-          {/* <CardMain /> */}
-          <BoxOffice />
-        </div>
+            <Routes>
+              <Route path='/' element={<Hello />} />
+              <Route path='/probs' element={<MyDiv1 />} />
+              <Route path='/card' element={<CardMain />} />
+              <Route path='/box' element={<BoxOffice />} />
+            </Routes>
+          </div>
 
-        <footer className="w-full h-16 
+          <footer className="w-full h-16 
                            flex justify-center items-center
                            bg-black text-slate-200">
-          <p className='text-sm font-bold'>
-            2024 여름 계절학기 소프트웨어융합기초1
-          </p>
-        </footer>
-      </main>
-    </div>
+            <p className='text-sm font-bold'>
+              2024 여름 계절학기 소프트웨어융합기초1
+            </p>
+          </footer>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
